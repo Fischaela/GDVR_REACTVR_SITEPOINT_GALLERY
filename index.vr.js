366999051cd5a85012de8d77247379da62e63150
Michaela Lehr <michaela.lehr@geildanke.com>
2017-04-22 11:37:31 +0200

    Remove scaffolded example code from index.vr.js

--- index.vr.js
+++ index.vr.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import {
   AppRegistry,
-  asset,
-  StyleSheet,
-  Pano,
-  Text,
   View,
 } from 'react-vr';
 
@@ -12,21 +8,6 @@ export default class GDVR_REACTVR_SITEPOINT_GALLERY extends React.Component {
   render() {
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
-        </Text>
       </View>
     );
   }
